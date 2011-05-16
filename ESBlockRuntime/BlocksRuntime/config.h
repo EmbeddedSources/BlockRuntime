7c806b0e5b7d41a5309cda42d9b796a8b43a51ab
Vladimir Gorbenko <volodg@mail.ru>
2011-05-16 15:30:18 +0300

    add sources files

---
+++ ESBlockRuntime/BlocksRuntime/config.h
@@ -0,0 +1,14 @@
+/*
+ *  config.h
+ *  ReutersInsider
+ *
+ *  Created by Vladimir Gorbenko on 1/21/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+//http://llvm.org/svn/llvm-project/compiler-rt/trunk/BlocksRuntime/
+
+#define HAVE_OSATOMIC_COMPARE_AND_SWAP_INT 1
+#define HAVE_OSATOMIC_COMPARE_AND_SWAP_LONG 1
+#define HAVE_LIBKERN_OSATOMIC_H 1
