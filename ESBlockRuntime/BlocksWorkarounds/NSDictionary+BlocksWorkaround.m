fdacb916ac2879d6ecd215abac65c97fcf66c556
volodg@mail.ru <volodg@mail.ru>
2011-09-12 23:10:02 +0300

    add -[ NSDictionary enumerateKeysAndObjectsUsingBlock: ] method support

--- ESBlockRuntime/BlocksWorkarounds/NSDictionary+BlocksWorkaround.m
+++ ESBlockRuntime/BlocksWorkarounds/NSDictionary+BlocksWorkaround.m
@@ -1,13 +1,60 @@
-//
-//  NSDictionary+BlocksWorkaround.m
-//  ESBlockRuntime
-//
-//  Created by vgor on 12.09.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+#import <Foundation/Foundation.h>
 
-#import "NSDictionary+BlocksWorkaround.h"
+#include <objc/runtime.h>
 
-@implementation NSDictionary (BlocksWorkaround)
+typedef void (^EnumerateDictionaryObjectsAndKeys) ( id key_, id obj_, BOOL* stop_ );
+
+@interface ESDictionaryBlockWorkarounds : NSObject
+
++(void)addMethodIfNeedWithSelector:( SEL )selector_
+                           toClass:( Class )class_;
+
+@end
+
+@implementation ESDictionaryBlockWorkarounds
+
++(void)addMethodIfNeedWithSelector:( SEL )selector_
+                           toClass:( Class )class_
+{
+   if ( ![ class_ instancesRespondToSelector: selector_ ] )
+   {
+      Method prototype_method_ = class_getInstanceMethod( self, selector_ );
+      const char* type_encoding_ = method_getTypeEncoding( prototype_method_ );
+      BOOL result_ = class_addMethod( class_
+                                     , selector_
+                                     , method_getImplementation( prototype_method_ )
+                                     , type_encoding_ );
+      NSAssert( result_, @"should be added" );
+   }
+}
+
+-(NSUInteger)countByEnumeratingWithState:( NSFastEnumerationState* )state_ objects:( id* )stackbuf_ count:( NSUInteger )len_
+{
+   [ self doesNotRecognizeSelector: _cmd ];
+   return 0;
+}
+
+-(id)objectForKey:( id )key_
+{
+   [ self doesNotRecognizeSelector: _cmd ];
+   return nil;
+}
+
+-(void)enumerateKeysAndObjectsUsingBlock:( EnumerateDictionaryObjectsAndKeys )block_
+{
+   BOOL stop_ = NO;
+   for ( id key_ in self )
+   {
+      block_( key_, [ self objectForKey: key_ ], &stop_ );
+      if ( stop_ )
+         break;
+   }
+}
+
++(void)load
+{
+   [ self addMethodIfNeedWithSelector: @selector( enumerateKeysAndObjectsUsingBlock: )
+                              toClass: [ NSDictionary class ] ];      
+}
 
 @end
